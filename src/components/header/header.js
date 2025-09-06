7fa4fe3bbde614df3cc96dd7a0bba0d344fc5e62
aartiipcollege <aarti.ggupta2004@gmail.com>
2025-09-06 12:20:24 +0530

    Change mail id

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -11,9 +11,9 @@ const Header = () => {
                 <li><a href='tel:+91-9258002828'>
                   <img src='/assests/img/phone.svg' alt='Phone Icon' />
                   +91-925 800 2828</a></li>
-                <li><a href='mailto:shivam.sharma@pharmmaex.com'>
+                <li><a href='mailto:info@pharmmaex.com'>
                   <img src='/assests/img/Message.svg' alt='Phone Icon' />
-                  shivam.sharma@pharmmaex.com</a></li>
+                  info@pharmmaex.com</a></li>
               </ul>
             </div>
             <div className='col-lg-6 col-md-6 col-sm-6  '>
